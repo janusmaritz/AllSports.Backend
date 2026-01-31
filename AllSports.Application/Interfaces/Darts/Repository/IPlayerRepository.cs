828a23a023b77d29a01ae5e78dd64e00e67cf972
Janus Maritz <janus.maritz7@gmail.com>
2026-01-31 08:35:45 +0200

    Setup darts database

---
+++ AllSports.Application/Interfaces/Darts/Repository/IPlayerRepository.cs
@@ -0,0 +1,9 @@
+﻿using AllSports.Domain.Entities.Darts;
+
+namespace AllSports.Application.Interfaces.Darts.Repository;
+
+public interface IPlayerRepository
+{
+    Task<int> AddPlayerAsync(PlayerProfile player);
+    Task<bool> PlayerExistsAsync(string name);
+}
---
+++ AllSports.Infrastructure/Repositories/Darts/PlayerRepository.cs
@@ -0,0 +1,28 @@
+﻿using AllSports.Application.Interfaces.Darts.Repository;
+using AllSports.Domain.Entities.Darts;
+using Microsoft.EntityFrameworkCore;
+using AllSports.Infrastructure.Persistence;
+
+namespace MyProject.Infrastructure.Repositories;
+
+public class PlayerRepository : IPlayerRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public PlayerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> AddPlayerAsync(PlayerProfile player)
+    {
+        _context.Players.Add(player);
+        await _context.SaveChangesAsync();
+        return player.Id;
+    }
+
+    public async Task<bool> PlayerExistsAsync(string name)
+    {
+        return await _context.Players.AnyAsync(p => p.FullName == name);
+    }
+}
